5a4cabab07ef8e9cbc731ceb565adcbf3e36f82a
John Cater <katre@henchmonkey.org>
2016-11-07 21:34:57 -0500

    Added db cleanup

--- db/init.sh
+++ db/init.sh
@@ -2,6 +2,7 @@ RDF_DIR=/home/katre/Downloads/cache/epub/
 DB_NAME=bibliodream.db
 
 pushd db
+rm $DB_NAME
 sqlite3 $DB_NAME < tables.sql
 find $RDF_DIR -name '*.rdf' | \
   xargs xsltproc gutenberg.xslt | \
