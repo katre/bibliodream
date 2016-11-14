e159cd46785183c1230bff538aa03ed953b8464b
John Cater <katre@henchmonkey.org>
2016-11-13 22:10:45 -0500

    Fixed quote usage

--- train.py
+++ train.py
@@ -19,23 +19,23 @@ from text_cnn import TextCNN
 # ==================================================
 
 # Data loading params
-tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training data to use for validation")
+tf.flags.DEFINE_float('dev_sample_percentage', .1, 'Percentage of the training data to use for validation')
 
 # Model Hyperparameters
-tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
-tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
-tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
-tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
-tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularizaion lambda (default: 0.0)")
+tf.flags.DEFINE_integer('embedding_dim', 128, 'Dimensionality of character embedding (default: 128)')
+tf.flags.DEFINE_string('filter_sizes', '3,4,5', 'Comma-separated filter sizes (default: '3,4,5')')
+tf.flags.DEFINE_integer('num_filters', 128, 'Number of filters per filter size (default: 128)')
+tf.flags.DEFINE_float('dropout_keep_prob', 0.5, 'Dropout keep probability (default: 0.5)')
+tf.flags.DEFINE_float('l2_reg_lambda', 0.0, 'L2 regularizaion lambda (default: 0.0)')
 
 # Training parameters
-tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_integer("num_epochs", 200, "Number of training epochs (default: 200)")
-tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
-tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
+tf.flags.DEFINE_integer('batch_size', 64, 'Batch Size (default: 64)')
+tf.flags.DEFINE_integer('num_epochs', 200, 'Number of training epochs (default: 200)')
+tf.flags.DEFINE_integer('evaluate_every', 100, 'Evaluate model on dev set after this many steps (default: 100)')
+tf.flags.DEFINE_integer('checkpoint_every', 100, 'Save model after this many steps (default: 100)')
 # Misc Parameters
-tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
-tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
+tf.flags.DEFINE_boolean('allow_soft_placement', True, 'Allow device soft device placement')
+tf.flags.DEFINE_boolean('log_device_placement', False, 'Log placement of ops on devices')
 
 FLAGS = tf.flags.FLAGS
 
@@ -47,25 +47,24 @@ def clean_str(string):
   Tokenization/string cleaning for all datasets except for SST.
   Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
   """
-  string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
-  string = re.sub(r"\'s", " \'s", string)
-  string = re.sub(r"\'ve", " \'ve", string)
-  string = re.sub(r"n\'t", " n\'t", string)
-  string = re.sub(r"\'re", " \'re", string)
-  string = re.sub(r"\'d", " \'d", string)
-  string = re.sub(r"\'ll", " \'ll", string)
-  string = re.sub(r",", " , ", string)
-  string = re.sub(r"!", " ! ", string)
-  string = re.sub(r"\(", " \( ", string)
-  string = re.sub(r"\)", " \) ", string)
-  string = re.sub(r"\?", " \? ", string)
-  string = re.sub(r"\s{2,}", " ", string)
+  string = re.sub(r'[^A-Za-z0-9(),!?\'\`]', ' ', string)
+  string = re.sub(r'\'s', ' \'s', string)
+  string = re.sub(r'\'ve', ' \'ve', string)
+  string = re.sub(r'n\'t', ' n\'t', string)
+  string = re.sub(r'\'re', ' \'re', string)
+  string = re.sub(r'\'d', ' \'d', string)
+  string = re.sub(r'\'ll', ' \'ll', string)
+  string = re.sub(r',', ' , ', string)
+  string = re.sub(r'!', ' ! ', string)
+  string = re.sub(r'\(', ' \( ', string)
+  string = re.sub(r'\)', ' \) ', string)
+  string = re.sub(r'\?', ' \? ', string)
+  string = re.sub(r'\s{2,}', ' ', string)
   return string.strip().lower()
 
 # Load data
 def load_data():
   """Load and return correctly labelled training data."""
-  print("Loading data...")
   x = []
   y = []
   for (text, subject) in gutenberg.load_data_and_label():
@@ -78,7 +77,7 @@ def load_data():
 # Build vocabulary
 def build_vocab(texts):
   """Build the vocabulary from the input texts."""
-  max_document_length = max([len(text.split(" ")) for text in texts])
+  max_document_length = max([len(text.split(' ')) for text in texts])
   vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
   texts = np.array(list(vocab_processor.fit_transform(texts)))
   return (texts, vocab_processor)
@@ -115,12 +114,12 @@ def train(x_train, y_train, x_dev, y_dev, vocab_processor):
           num_classes=y_train.shape[1],
           vocab_size=len(vocab_processor.vocabulary_),
           embedding_size=FLAGS.embedding_dim,
-          filter_sizes=list(map(int, FLAGS.filter_sizes.split(","))),
+          filter_sizes=list(map(int, FLAGS.filter_sizes.split(','))),
           num_filters=FLAGS.num_filters,
           l2_reg_lambda=FLAGS.l2_reg_lambda)
 
       # Define Training procedure
-      global_step = tf.Variable(0, name="global_step", trainable=False)
+      global_step = tf.Variable(0, name='global_step', trainable=False)
       optimizer = tf.train.AdamOptimizer(1e-3)
       grads_and_vars = optimizer.compute_gradients(cnn.loss)
       train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
@@ -129,40 +128,40 @@ def train(x_train, y_train, x_dev, y_dev, vocab_processor):
       grad_summaries = []
       for g, v in grads_and_vars:
         if g is not None:
-          grad_hist_summary = tf.histogram_summary("{}/grad/hist".format(v.name), g)
-          sparsity_summary = tf.scalar_summary("{}/grad/sparsity".format(v.name), tf.nn.zero_fraction(g))
+          grad_hist_summary = tf.histogram_summary('{}/grad/hist'.format(v.name), g)
+          sparsity_summary = tf.scalar_summary('{}/grad/sparsity'.format(v.name), tf.nn.zero_fraction(g))
           grad_summaries.append(grad_hist_summary)
           grad_summaries.append(sparsity_summary)
       grad_summaries_merged = tf.merge_summary(grad_summaries)
 
       # Output directory for models and summaries
       timestamp = str(int(time.time()))
-      out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
-      print("Writing to {}\n".format(out_dir))
+      out_dir = os.path.abspath(os.path.join(os.path.curdir, 'runs', timestamp))
+      print('Writing to {}\n'.format(out_dir))
 
       # Summaries for loss and accuracy
-      loss_summary = tf.scalar_summary("loss", cnn.loss)
-      acc_summary = tf.scalar_summary("accuracy", cnn.accuracy)
+      loss_summary = tf.scalar_summary('loss', cnn.loss)
+      acc_summary = tf.scalar_summary('accuracy', cnn.accuracy)
 
       # Train Summaries
       train_summary_op = tf.merge_summary([loss_summary, acc_summary, grad_summaries_merged])
-      train_summary_dir = os.path.join(out_dir, "summaries", "train")
+      train_summary_dir = os.path.join(out_dir, 'summaries', 'train')
       train_summary_writer = tf.train.SummaryWriter(train_summary_dir, sess.graph)
 
       # Dev summaries
       dev_summary_op = tf.merge_summary([loss_summary, acc_summary])
-      dev_summary_dir = os.path.join(out_dir, "summaries", "dev")
+      dev_summary_dir = os.path.join(out_dir, 'summaries', 'dev')
       dev_summary_writer = tf.train.SummaryWriter(dev_summary_dir, sess.graph)
 
       # Checkpoint directory. Tensorflow assumes this directory already exists so we need to create it
-      checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
-      checkpoint_prefix = os.path.join(checkpoint_dir, "model")
+      checkpoint_dir = os.path.abspath(os.path.join(out_dir, 'checkpoints'))
+      checkpoint_prefix = os.path.join(checkpoint_dir, 'model')
       if not os.path.exists(checkpoint_dir):
         os.makedirs(checkpoint_dir)
       saver = tf.train.Saver(tf.all_variables())
 
       # Write vocabulary
-      vocab_processor.save(os.path.join(out_dir, "vocab"))
+      vocab_processor.save(os.path.join(out_dir, 'vocab'))
 
       # Initialize all variables
       sess.run(tf.initialize_all_variables())
@@ -180,7 +179,7 @@ def train(x_train, y_train, x_dev, y_dev, vocab_processor):
             [train_op, global_step, train_summary_op, cnn.loss, cnn.accuracy],
             feed_dict)
         time_str = datetime.datetime.now().isoformat()
-        print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+        print('{}: step {}, loss {:g}, acc {:g}'.format(time_str, step, loss, accuracy))
         train_summary_writer.add_summary(summaries, step)
 
       def dev_step(x_batch, y_batch, writer=None):
@@ -196,7 +195,7 @@ def train(x_train, y_train, x_dev, y_dev, vocab_processor):
             [global_step, dev_summary_op, cnn.loss, cnn.accuracy],
             feed_dict)
         time_str = datetime.datetime.now().isoformat()
-        print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+        print('{}: step {}, loss {:g}, acc {:g}'.format(time_str, step, loss, accuracy))
         if writer:
           writer.add_summary(summaries, step)
 
@@ -209,12 +208,12 @@ def train(x_train, y_train, x_dev, y_dev, vocab_processor):
         train_step(x_batch, y_batch)
         current_step = tf.train.global_step(sess, global_step)
         if current_step % FLAGS.evaluate_every == 0:
-          print("\nEvaluation:")
+          print('\nEvaluation:')
           dev_step(x_dev, y_dev, writer=dev_summary_writer)
-          print("")
+          print('')
         if current_step % FLAGS.checkpoint_every == 0:
           path = saver.save(sess, checkpoint_prefix, global_step=current_step)
-          print("Saved model checkpoint to {}\n".format(path))
+          print('Saved model checkpoint to {}\n'.format(path))
 
 def batch_iter(data, batch_size, num_epochs, shuffle=True):
   """
@@ -237,23 +236,40 @@ def batch_iter(data, batch_size, num_epochs, shuffle=True):
 
 # Main run function.
 def main(argv=None):
-  print("\nParameters:")
+  print('\nParameters:')
   for attr, value in sorted(FLAGS.__flags.items()):
-    print("{}={}".format(attr.upper(), value))
-  print("")
+    print('{}={}'.format(attr.upper(), value))
+  print('')
 
   # Train on the data.
+  print('Loading data...')
   x, y = load_data()
-  for i in xrange(len(x)):
-    print('Text: %s' % x[i][:100])
-    print('Subject: %s' % y[i])
-  return
+  #for i in xrange(len(x)):
+  #  print('Text: %s' % x[i][:100])
+  #  print('Subject: %s' % y[i])
+  #return
 
+  print('Extracting vocabulary...')
   x, vocab_processor = build_vocab(x)
+  #for i in xrange(len(x)):
+  #  print('Text: %s' % x[i][:100])
+  #return
+
+  print('Shuffling data...')
   x, y = shuffle_data(x, y)
+  #for i in xrange(len(x)):
+  #  print('Text: %s' % x[i][:100])
+  #  print('Subject: %s' % y[i])
+  #return
+
+  print('Splitting data...')
   ((x_train, y_train), (x_dev, y_dev)) = split_data(x, y)
-  print("Vocabulary Size: {:d}".format(len(vocab_processor.vocabulary_)))
-  print("Train/Dev split: {:d}/{:d}".format(len(y_train), len(y_dev)))
+
+  # Summarize data.
+  print('Vocabulary Size: {:d}'.format(len(vocab_processor.vocabulary_)))
+  print('Train/Dev split: {:d}/{:d}'.format(len(y_train), len(y_dev)))
+
+  print('Training...')
   train(x, y, vocab_processor)
 
 if __name__ == '__main__':
