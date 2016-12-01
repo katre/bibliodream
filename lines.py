6542ee9fadc5eebb175fc6ade1a94b07c27f320f
John Cater <katre@henchmonkey.org>
2016-11-30 22:13:47 -0500

    Added basic book/line selection and counting

--- lines.py
+++ lines.py
@@ -6,6 +6,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import random
+import re
+
 from tensorflow.python.platform import flags
 from tensorflow.python.platform import gfile
 
@@ -18,6 +21,21 @@ flags.DEFINE_integer('book_count', 10, 'Number of books to use.')
 flags.DEFINE_string('output', 'book.txt', 'Filename to write to.')
 
 # Functions to get books and lines.
+def get_random_book(books):
+  return random.choice(books)
+
+END_PUNCTUATION = re.compile(r'[.?!]')
+
+def get_random_line(book):
+  # Split text on ending punctuation
+  lines = END_PUNCTUATION.split(book.data)
+  line = random.choice(lines) + '.'
+  # Remove any newlines.
+  return line.replace('\n', '')
+
+def count_words(line):
+  words = line.split()
+  return len(words)
 
 def main(argv=None):
   data = gutenberg.GutenbergData(0, FLAGS.book_count, random=True)
@@ -27,9 +45,20 @@ def main(argv=None):
 
   print('Found %d books' % len(books))
 
+  total_words = 0
   with open(FLAGS.output, 'w') as f:
-    # Output lines.
-    pass
+    while total_words < FLAGS.word_count:
+      # Pick a random line from a random book.
+      book = get_random_book(books)
+      line = get_random_line(book)
+
+      # Count the words.
+      new_words = count_words(line)
+      total_words += new_words
+
+      # Write the line.
+      f.write(line)
+
 
 if __name__ == '__main__':
   from tensorflow.python.platform import app
