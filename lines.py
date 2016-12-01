1298a682a8e1adcb9084d904af2f1b9af622beaa
John Cater <katre@henchmonkey.org>
2016-11-30 22:00:28 -0500

    Added book lookup logic.

--- lines.py
+++ lines.py
@@ -9,6 +9,8 @@ from __future__ import print_function
 from tensorflow.python.platform import flags
 from tensorflow.python.platform import gfile
 
+import gutenberg
+
 FLAGS = flags.FLAGS
 
 flags.DEFINE_integer('word_count', 100, 'Target word count for the output.')
@@ -18,7 +20,15 @@ flags.DEFINE_string('output', 'book.txt', 'Filename to write to.')
 # Functions to get books and lines.
 
 def main(argv=None):
-  with open(FLAGS.output) as f:
+  data = gutenberg.GutenbergData(0, FLAGS.book_count, random=True)
+
+  # Find the books.
+  books = data.books
+
+  print('Found %d books' % len(books))
+
+  with open(FLAGS.output, 'w') as f:
+    # Output lines.
     pass
 
 if __name__ == '__main__':
